f9feabd0304c20a6750035b96654c00372512172
Ronaldo Hauser Dias <duqueronaldo@icloud.com>
2024-06-11 22:07:52 -0300

    backend_modulo32

---
+++ src/com.exemple/Produto.java
@@ -0,0 +1,59 @@
+package com.exemple;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+    private int quantidade;
+    private String categoria;
+
+    // Construtores, getters e setters
+
+    public Produto() {}
+
+    public Produto(String descricao, int quantidade, String categoria) {
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.categoria = categoria;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
